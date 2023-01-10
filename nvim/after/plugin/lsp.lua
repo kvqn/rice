ab9f26e24a05543f5bfcb1149cb81083b133b6ab
kvqn <guneetaggarwal@gmail.com>
2023-01-10 15:10:22 +0530

    nvim : lsp show diagnostic

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,3 +6,12 @@ lsp.preset("recommended")
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
