c69eb249e20ee66a6e0692af9c125c16a787f5df
kvqn <guneetaggarwal@gmail.com>
2023-01-11 19:02:46 +0530

    fixed snippets

---
+++ nvim/after/plugin/snippets/init.lua
@@ -0,0 +1,71 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+
+ls.setup({
+	history = true,
+	-- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+	-- Snippets aren't automatically removed if their text is deleted.
+	-- `delete_check_events` determines on which events (:h events) a check for
+	-- deleted snippets is performed.
+	-- This can be especially useful when `history` is enabled.
+	delete_check_events = "TextChanged",
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "choiceNode", "Comment" } },
+			},
+		},
+	},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = true,
+	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+	store_selection_keys = "<Tab>",
+	-- luasnip uses this function to get the currently active filetype. This
+	-- is the (rather uninteresting) default, but it's possible to use
+	-- eg. treesitter for getting the current filetype by setting ft_func to
+	-- require("luasnip.extras.filetype_functions").from_cursor (requires
+	-- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+	-- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+	ft_func = function()
+		return vim.split(vim.bo.filetype, ".", true)
+	end,
+})
+
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+-- require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets/" })
+require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/after/plugin/snippets/snips-vscode/" })
+
+-- Load snippets
+
--- nvim/snippets/py.lua
+++ nvim/snippets/py.lua
@@ -1,10 +1,11 @@
 
 local luasnip = require "luasnip"
+local t = luasnip.text_node
 
 return {
 
-    luasnip.snippt(
-        { trig = "selenium-imports" },
+    luasnip.snippet(
+        "selenium-imports",
         { t({
             "from selenium import webdriver",
             "from selenium.webdriver.common.by import By",
@@ -12,6 +13,18 @@ return {
             "from selenium.webdriver.support import expected_conditions as EC",
         }) }
     )
+
+    -- , luasnip.snippet(
+
+    --     { trig = "logging-format" },
+    --     { t({
+    --         "if args.verbose:",
+    --             "logging.basicConfig(level=logging.DEBUG, force=True)",
+    --         "else:",
+    --             "logging.basicConfig(level=logging.INFO, force=True)",
+    --         "logging.basicConfig(format='[ %(asctime)s - %(levelname)s - %(name)s ] %(message)s', force=True)",
+    --     }) }
+    -- )
 
 }
 
