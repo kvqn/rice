ab192d9679e6cfe87a2f19f504bf05951d0bf3d3
kvqn <guneetaggarwal@gmail.com>
2023-01-17 15:45:55 +0530

    nvim : harpoon keybinds

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,4 +5,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>hv", ui.toggle_quick_menu)
 
+vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<M-5>", function() ui.nav_file(5) end)
+
 
