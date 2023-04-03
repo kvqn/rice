db73e785d7c8edeb062a2e8ca8eb29d1afa12885
kvqn <guneetaggarwal@gmail.com>
2023-04-03 23:18:06 +0530

    nvim + zsh : ripgrep is god

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,6 @@
 
 local telescope = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>pf", telescope.find_files, {})
-vim.keymap.set("n", "<leader>pg", telescope.live_grep, {})
-
 require('telescope').setup {
     defaults = {
         mappings = {
@@ -12,18 +9,19 @@ require('telescope').setup {
                 ["<C-k>"] = require('telescope.actions').move_selection_previous,
             },
         },
-
-        file_ignore_patterns = {
-            "^node_modules$",
-            "^out$",
-            "^__pycache__$",
-            "^.git$"
-        },
     },
 
     pickers = {
         find_files = {
-            hidden = true
+            find_command = {'rg', '--files'},
+            hidden=true,
+        },
+        live_grep = {
+            find_command = {'rg'},
+            hidden=true,
         }
     }
 }
+
+vim.keymap.set("n", "<leader>pf", telescope.find_files, {})
+vim.keymap.set("n", "<leader>pg", telescope.live_grep, {})
