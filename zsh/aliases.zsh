6d46371000313655ef2615f421c0826752970177
kvqn <guneetaggarwal@gmail.com>
2023-04-06 20:17:35 +0530

    aliases.zsh indentation fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,26 +1,29 @@
 # Aliases
 
- alias ls="exa --icons"
- alias l="ls -lh"
- alias ll="ls -lah"
- alias sudo="sudo --preserve-env"
- alias less="less -I" # ignore case
+alias ls="exa --icons"
+alias l="ls -lh"
+alias ll="ls -lah"
+alias sudo="sudo --preserve-env"
+alias less="less -I" # ignore case
 
- alias todo="vim /home/kevqn/projects/todo" # very useful
- alias tree="tree -I __pycache__"
- alias py='python' # im lazy af
+alias todo="vim /home/kevqn/projects/todo" # very useful
+alias tree="tree -I __pycache__"
+alias py='python' # im lazy af
 
- alias v="vim" # im lazy af
+alias v="vim" # im lazy af
 
- alias vim="nvim"
- alias tmux="tmux -u"
+alias vim="nvim"
+alias tmux="tmux -u"
 
- alias gs="git status --short" # use it a lot
- alias gl="git log --oneline" # short and sweet
+alias gs="git status --short" # use it a lot
+alias gl="git log --oneline" # short and sweet
 
- alias yayclean="yay -Sc" # deletes old packages
+alias yayclean="yay -Sc" # deletes old packages
 
 
- # Programs
- alias cgpa="python /home/kevqn/projects/cgpa-analysis/cgpa"
- alias vim-plugins-profile="python /home/kevqn/projects/bin/vim-plugins-profile/vim-plugins-profile.py /usr/bin/nvim"
+# Programs
+alias cgpa="python /home/kevqn/projects/cgpa-analysis/cgpa"
+alias vim-plugins-profile="python /home/kevqn/projects/bin/vim-plugins-profile/vim-plugins-profile.py /usr/bin/nvim"
+
+# Fizes
+alias pip="noglob pip"
