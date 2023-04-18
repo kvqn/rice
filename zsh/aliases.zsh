2603024e3bed3bea5cc2a2d041bc56b30a7c1c75
kvqn <guneetaggarwal@gmail.com>
2023-04-18 21:49:55 +0530

    zsh : alias changes (gl)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,7 +16,7 @@ alias vim="nvim"
 alias tmux="tmux -u"
 
 alias gs="git status --short" # use it a lot
-alias gl="git log --oneline" # short and sweet
+alias gl="git log --oneline -25" # short and sweet
 
 alias yayclean="yay -Sc" # deletes old packages
 
