cbcac5a61bc88a59d3e912a806586e582f5fd69a
kvqn <guneetaggarwal@gmail.com>
2023-05-17 12:10:40 +0530

    zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,11 @@
 # Aliases
 
+# listing files
 alias ls="exa --icons"
 alias l="ls -lh"
 alias ll="ls -lah"
+alias la="ls -a"
+
 alias less="less -I" # ignore case
 
 alias todo="vim /home/kevqn/projects/todo" # very useful
@@ -19,6 +22,13 @@ alias gl="git log --oneline -25" # short and sweet
 
 alias yayclean="yay -Sc" # deletes old packages
 
+# vim
+alias vzsh="v ~/.zshrc"
+alias valias="v ~/projects/rice/zsh/aliases.zsh"
+alias vvim="v ~/.config/nvim"
+alias vvimrc="v ~/.config/nvim"
+alias vxmonad="v ~/.xmonad/xmonad.hs"
+
 
 # Programs
 alias cgpa="python /home/kevqn/projects/cgpa-analysis/cgpa"
@@ -29,3 +39,7 @@ alias pip="noglob pip"
 
 # Complex Aliases
 sudo() { /usr/bin/sudo -E zsh -ic "$*" }
+
+alias java8="/usr/lib/jvm/java-8-openjdk/bin/java"
+alias java17="/usr/lib/jvm/java-17-openjdk/bin/java"
+alias java20="/usr/lib/jvm/java-20-openjdk/bin/java"
