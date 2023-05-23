be3074bdd2cb16c7765cef28737a5887d71be254
kvqn <guneetaggarwal@gmail.com>
2023-05-23 22:53:30 +0530

    zsh: zsh autoenv changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,10 @@
 # Aliases
 
+function reload-aliases() {
+  source ~/projects/rice/zsh/aliases.zsh
+  echo "Reloaded aliases"
+}
+
 # listing files
 alias ls="exa --icons"
 alias l="ls -lh"
@@ -22,12 +27,22 @@ alias gl="git log --oneline -25" # short and sweet
 
 alias yayclean="yay -Sc" # deletes old packages
 
+function cheatsheet() {
+  if [ -z "$1" ]; then
+    echo "Usage: cheatsheet <command>"
+    return 1
+  fi
+  curl cht.sh/$1 | less -R
+}
+alias cheat="cheatsheet"
+alias chtsh="cheatsheet"
+
 # vim
-alias vzsh="v ~/.zshrc"
-alias valias="v ~/projects/rice/zsh/aliases.zsh"
-alias vvim="v ~/.config/nvim"
-alias vvimrc="v ~/.config/nvim"
-alias vxmonad="v ~/.xmonad/xmonad.hs"
+alias vzsh="cd ~/projects/rice/zsh; v .zshrc"
+alias valias="cd ~/projects/rice/zsh; v aliases.zsh"
+alias vvim="cd ~/projects/rice/nvim; v ."
+alias vvimrc="vvim"
+alias vxmonad="cd ~/projects/rice/xmonad; v xmonad.hs"
 
 
 # Programs
@@ -40,6 +55,10 @@ alias pip="noglob pip"
 # Complex Aliases
 sudo() { /usr/bin/sudo -E zsh -ic "$*" }
 
+# java is stupid
 alias java8="/usr/lib/jvm/java-8-openjdk/bin/java"
 alias java17="/usr/lib/jvm/java-17-openjdk/bin/java"
 alias java20="/usr/lib/jvm/java-20-openjdk/bin/java"
+
+# webd shit
+export react="127.0.0.1:3000"
