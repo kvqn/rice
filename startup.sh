8f48d0d25b47818c905cdd8ba04900eeccc50da2
kvqn <guneetaggarwal@gmail.com>
2023-06-06 00:40:28 +0530

    xmonad
    changed some redundant startup apps / commands

--- startup.sh
+++ startup.sh
@@ -3,10 +3,10 @@
 # Stopping the annoying beeps
 sudo rmmod pcspkr
 
-# Launching startup applications
-qbittorrent > /dev/null &
+# # Launching startup applications
+# qbittorrent > /dev/null &
 mpris-proxy > /dev/null &
-emote > /dev/null &
+# emote > /dev/null &
 
 # Launching panels and stuff
 bash ~/.xmonad/bin/autostart.sh
--- xmonad/bin/autostart.sh
+++ xmonad/bin/autostart.sh
@@ -5,7 +5,7 @@
 ## Autostart Programs
 
 # Kill already running process
-_ps=(picom dunst ksuperkey mpd xfce-polkit xfce4-power-manager)
+_ps=(picom dunst mpd xfce4-power-manager)
 for _prs in "${_ps[@]}"; do
 	if [[ `pidof ${_prs}` ]]; then
 		killall -9 ${_prs}
@@ -16,14 +16,10 @@ done
 xsetroot -cursor_name left_ptr
 
 # Polkit agent
-/usr/lib/xfce-polkit/xfce-polkit &
+# /usr/lib/xfce-polkit/xfce-polkit &
 
 # Enable power management
 xfce4-power-manager &
-
-# Enable Super Keys For Menu
-ksuperkey -e 'Super_L=Alt_L|F1' &
-ksuperkey -e 'Super_R=Alt_L|F1' &
 
 # Restore wallpaper
 hsetroot -cover ~/.xmonad/wallpaper.png
