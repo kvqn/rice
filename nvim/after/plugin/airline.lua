d1d7a72d8588ba2bfc36e159935a3f46cdb63695
kvqn <guneetaggarwal@gmail.com>
2023-06-08 12:23:49 +0530

    nvim
    airline font fixing

--- nvim/after/plugin/airline.lua
+++ nvim/after/plugin/airline.lua
@@ -1,5 +1,8 @@
 -- print("Hello from airline")
 
 vim.cmd([[
-let g:airline_powerline_fonts = 1
+let g:airline_symbols.branch = ''
+let g:airline_symbols.colnr = ' '
+let g:airline_symbols.linenr = ' '
+let g:airline_symbols.maxlinenr = ''
 ]])
