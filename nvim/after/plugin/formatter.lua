49582dcaa71eb1bdd8b30388200a03402bf97082
kvqn <guneetaggarwal@gmail.com>
2023-06-08 08:04:28 +0000

    nvim
    formatter - configured markdown and lua

--- nvim/after/plugin/formatter.lua
+++ nvim/after/plugin/formatter.lua
@@ -1,17 +1,50 @@
-local formatter = require "formatter"
+local formatter = require("formatter")
 
-formatter.setup {
-  filetype = {
-      typescriptreact = {
-        function()
-          return {
-            exe = "npx prettier",
-            args = {
-              "--parser typescript",
-            },
-            stdin = true
-          }
-        end
-        }
-    }
-  }
+formatter.setup({
+	filetype = {
+		typescriptreact = {
+			function()
+				return {
+					exe = "npx prettier",
+					args = {
+						"--parser typescript",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		typescript = {
+			function()
+				return {
+					exe = "npx prettier",
+					args = {
+						"--parser typescript",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		markdown = {
+			function()
+				return {
+					exe = "npx prettier",
+					args = {
+						"--parser markdown",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		lua = {
+			function()
+				return {
+					exe = "stylua",
+					args = {
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+	},
+})
