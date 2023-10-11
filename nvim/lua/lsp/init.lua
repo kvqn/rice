d18d34ee3aab6a3e0b548665d2392ffe98f8c2df
kvqn <guneetaggarwal@gmail.com>
2023-10-12 00:08:54 +0530

    nvim
    fixed lsp warnings

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,15 +1,29 @@
 
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
+-- lsp.preset("recommended")
 
-lsp.nvim_workspace()
+-- lsp.nvim_workspace()
 
 -- Commenting this line because it gives me pain.
 -- I don't use pylsp anymore but I still want to keep my configuration if I ever wind up using it again.
 -- Keeping this line uncommented gives me a warning every time I open a python file.
 -- I don't know how to fix that while still keeping the configuration.
 -- lsp.configure('pylsp', require 'lsp.pylsp')
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp.default_setup,
+  },
+})
 
 lsp.setup()
 
